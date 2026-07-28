1e9f536b3c8920680f13d0a538b4f66acf1d6874
Syed Abbuzar <syedabbuzar777@gmail.com>
2026-07-29 03:49:12 +0530

    updated

--- index.js
+++ index.js
@@ -72,12 +72,12 @@ app.get("/", (req, res) => {
   res.send("🚀 Star Ice Cream Parlour Backend is Running...");
 });
 
-// ========================
-// Server
-// ========================
+if (process.env.VERCEL !== "1") {
+  const PORT = process.env.PORT || 5000;
 
-const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`✅ Server is running on http://localhost:${PORT}`);
+  });
+}
 
-app.listen(PORT, () => {
-  console.log(`✅ Server is running on http://localhost:${PORT}`);
-});
+export default app;
